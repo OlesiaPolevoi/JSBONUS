610c1692c49544172876aba4d3f2bb4f1d447882
Olesia Polevoi <olesia.polevoi@gmail.com>
2021-12-21 16:58:09 -0500

    lesson04 updated

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 "use strict";
-console.log("hello");
+const workingHard = function (variable) {
+  if (typeof variable !== "string") {
+    return "В качестве аргумента передана не строка.";
+  }
+
+  let variable1 = variable.trim();
+  if (variable1.length <= 30) {
+    return variable1;
+  }
+
+  return variable1.substring(0, 30) + "...";
+};
+
+// const numInput = workingHard(123);
+// console.log(numInput);
+
+const strInput = workingHard(" Hello beautiful people of this earth  ");
+console.log(strInput);
