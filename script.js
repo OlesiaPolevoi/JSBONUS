107129ec01c23e544c87b4b7999187fb2f81c62e
Olesia Polevoi <olesia.polevoi@gmail.com>
2021-12-21 16:48:32 -0500

    bonus lesson02 complete

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 "use strict";
-console.log("hello");
+//lesson02
+
+const num = 266219;
+const numToString = num.toString();
+// console.log(numToString);
+const stringToArray = numToString.split("");
+// console.log(stringToArray);
+
+const arrayToNumberArray = stringToArray.map(Number);
+// console.log(arrayToNumberArray);
+let result = 1;
+// console.log(result);
+for (let i = 0; i < arrayToNumberArray.length; i++) {
+  result = result * arrayToNumberArray[i];
+}
+console.log(result);
+const resultTo3rdPower = result ** 3;
+console.log(resultTo3rdPower);
+const resultToString = resultTo3rdPower.toString();
+console.log(resultToString.slice(0, 2));
