e1aebf023fb4f5626aa51aa18343d582723a4c25
Olesia Polevoi <olesia.polevoi@gmail.com>
2021-12-23 21:54:14 -0500

    lesson06 in prgress

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 "use strict";
+
+let usersNum;
+console.log(usersNum);
+
+function generateRandomNumber(min, max) {
+  let randomNum = Math.ceil(Math.random() * (max - min + 1) + min);
+  console.log(console.log(randomNum));
+
+  function two() {
+    usersNum = +prompt("Guess a number from 1 - 100");
+    console.log(usersNum);
+
+    if (usersNum > randomNum) {
+      alert("The number you are guessing is smaller");
+      two();
+    }
+    if (usersNum < randomNum) {
+      alert("The number you are guessing is bigger");
+      two();
+    }
+    if (usersNum === randomNum) {
+      alert("Congratulations! You've won!");
+      return;
+    }
+  }
+  two();
+}
+
+generateRandomNumber(1, 100);
