23c93fd8d0e1780d1900b1712626c1ee7b2ee256
Olesia Polevoi <olesia.polevoi@gmail.com>
2021-12-23 22:10:55 -0500

    commit

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 "use strict";
 
 let usersNum;
-console.log(usersNum);
 
 function generateRandomNumber(min, max) {
   let randomNum = Math.ceil(Math.random() * (max - min + 1) + min);
-  console.log(console.log(randomNum));
+  console.log(randomNum);
 
   function two() {
     usersNum = +prompt("Guess a number from 1 - 100");
