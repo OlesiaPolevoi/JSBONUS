d213862a8d6a905bbd06929108febf85e1b45c0e
Olesia Polevoi <olesia.polevoi@gmail.com>
2021-12-23 22:39:21 -0500

    commit lesson6

--- script.js
+++ script.js
@@ -8,7 +8,17 @@ function generateRandomNumber(min, max) {
 
   function two() {
     usersNum = +prompt("Guess a number from 1 - 100");
-    console.log(usersNum);
+    console.log(usersNum == null);
+
+    if (usersNum == null) {
+      alert("The game is over");
+      return;
+    }
+
+    if (isNaN(parseFloat(usersNum)) || usersNum === "" || usersNum == 0) {
+      alert("You should enter a number from 1 - 100 only");
+      two();
+    }
 
     if (usersNum > randomNum) {
       alert("The number you are guessing is smaller");
@@ -20,7 +30,6 @@ function generateRandomNumber(min, max) {
     }
     if (usersNum === randomNum) {
       alert("Congratulations! You've won!");
-      return;
     }
   }
   two();
