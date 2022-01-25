3725903823b44bc4462508037fc93ea9d18f8b15
Olesia Polevoi <olesia.polevoi@gmail.com>
2022-01-24 21:24:52 -0500

    lesson 26 done

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 "use strict";
+
+const getData = () => {
+  return new Promise((resolve, reject) => {
+    fetch("db.json")
+      .then((response) => response.json())
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+};
+
+const sendData = (data) => {
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
+};
+
+getData().then((data) => sendData(data));
